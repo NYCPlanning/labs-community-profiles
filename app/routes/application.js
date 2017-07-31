2a5893f9da9a2f95015f401d97a996455e93966e
allthesignals <wilburnforce@gmail.com>
2017-07-31 17:33:31 -0400

    Set up basic SQL query for listing of CD

---
+++ app/routes/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+const SQL = 'SELECT a.borocd AS borocd, b.boroname AS boroname, a.the_geom FROM ny_community_districts a INNER JOIN ny_boroughs b ON ST_Intersects(ST_Centroid(a.the_geom), b.the_geom)';
+const ENDPOINT = `https://cartoprod.capitalplanning.nyc/user/cpp/api/v2/sql?q=${SQL}&format=geojson`;
+
+export default Ember.Route.extend({
+  model() {
+    return $.getJSON(ENDPOINT);
+  }
+});
