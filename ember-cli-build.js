941cf71d2b52ca07e9315f68f9eb6d937b374678
allthesignals <wilburnforce@gmail.com>
2017-07-31 17:32:48 -0400

    Include on necessary parts of plugins

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      exclude: [
+        'images/layers-2x.png',
+        'images/layers.png',
+        'images/marker-icon-2x.png',
+        'images/marker-icon.png',
+        'images/marker-shadow.png'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
