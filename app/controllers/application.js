ad473f7b89e2bed420394f9f014773dcf40b5f83
allthesignals <wilburnforce@gmail.com>
2017-08-01 12:22:09 -0400

    Explicitly define controller properties

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
   lat: 40.7071266,
   lng: -74,
   zoom: 10,
+  'tooltip-text': '',
   actions: {
     handleClick(e) {
       const { boro, borocd } = e.layer.feature.properties;
