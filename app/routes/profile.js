2d9a30a611bcb74d15ae019263d2348f9b2a518c
allthesignals <wilburnforce@gmail.com>
2017-07-31 22:08:02 -0400

    Clean up syntax

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 const HOST = 'https://cartoprod.capitalplanning.nyc/user/cpp/api/v2/sql';
-const JOINSQL = 'SELECT a.borocd AS borocd, b.boroname AS boroname, a.the_geom FROM ny_community_districts a INNER JOIN ny_boroughs b ON ST_Intersects(ST_Centroid(a.the_geom), b.the_geom)';
+const JOINSQL = ' SELECT a.borocd AS borocd, b.boroname AS boroname, a.the_geom \
+                  FROM ny_community_districts a \
+                  INNER JOIN ny_boroughs b ON ST_Intersects(ST_Centroid(a.the_geom), b.the_geom)';
 
 export default Ember.Route.extend({
   model(params) {
