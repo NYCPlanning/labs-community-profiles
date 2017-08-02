2abad50f4d2f0181b9593d0ce6d79fa4c5293b11
allthesignals <wilburnforce@gmail.com>
2017-08-02 11:55:46 -0400

    Generate option list for ember power select

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,6 +6,18 @@ export default Ember.Controller.extend({
   lng: -74,
   zoom: 10,
   'tooltip-text': '',
+
+  options: Ember.computed('model.features.@each', function() {
+    let features = this.get('model.features');
+    return features.map(feature=> { 
+      return { 
+        name: `${feature.properties.boro} ${feature.properties.cd}`, 
+        value: feature.properties.boro,
+        full: feature.properties.borocd,
+      } 
+    });
+  }),
+
   style: Ember.computed('mapState.currentlySelected', function() {
     return (geoJsonFeature) => {
       if(geoJsonFeature.properties.borocd == this.get('mapState.currentlySelected')) {
@@ -15,10 +27,15 @@ export default Ember.Controller.extend({
       }
     }
   }),
+  
   actions: {
     handleClick(e) {
       const { boro, borocd } = e.layer.feature.properties;
       this.transitionToRoute('profile', boro.dasherize(), borocd);
+    },
+
+    handleTest() {
+      this.set('selected', 'Bronx 4');
     },
 
     handleMouseover(e) {
