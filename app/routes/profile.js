ceb5d7e763c073f60fc80b7445604fa41b44aa4d
allthesignals <wilburnforce@gmail.com>
2017-08-03 13:53:17 -0400

    Fetch data profile data
    Only get the necessary data

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -6,17 +6,28 @@ import carto from '../utils/carto';
 export default Ember.Route.extend({
   mapState: Ember.inject.service(),
   model(params) {
-    return this.modelFor('application')
-      .features.find(district=>district.properties.borocd === parseInt(params.name));
+    const sql = `SELECT * FROM community_district_profiles WHERE borocd=${params.name}`;
+    const endpoint = `https://cartoprod.capitalplanning.nyc/user/cpp/api/v2/sql?q=${sql}`;
+
+    const selectedDistrict = this.modelFor('application')
+      .features
+      .find(district => district.properties.borocd === parseInt(params.name, 10));
+
+    return fetch(endpoint)
+      .then(response => response.json())
+      .then((json) => {
+        selectedDistrict.properties.dataprofile = json.rows[0];
+        return selectedDistrict;
+      });
   },
   afterModel(profile) {
-    let mapState = this.get('mapState');
+    const mapState = this.get('mapState');
     mapState.set('bounds', L.geoJson(profile.geometry).getBounds());
     mapState.set('geom', profile.geometry);
 
     carto.getTileTemplate()
-      .then(landUseTemplate => {
+      .then((landUseTemplate) => {
         mapState.set('landUseTemplate', landUseTemplate);
       });
-  }
+  },
 });
