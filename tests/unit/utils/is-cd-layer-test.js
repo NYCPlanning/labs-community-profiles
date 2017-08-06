32bdb1d6b5319d10f6e2c8d0dfec787a16128022
allthesignals <wilburnforce@gmail.com>
2017-08-05 20:26:54 -0400

    Test util is-cd-layer

---
+++ tests/unit/utils/is-cd-layer-test.js
@@ -0,0 +1,10 @@
+import isCdLayer from 'labs-community-portal/utils/is-cd-layer';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | is cd layer');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = isCdLayer();
+  assert.ok(result);
+});
