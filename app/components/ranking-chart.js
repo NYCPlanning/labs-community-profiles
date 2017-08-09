1c891aa353cf3a4339316138db88180f1502e834
allthesignals <wilburnforce@gmail.com>
2017-08-09 15:15:43 -0400

    Add tooltips, colors, transitions, selections

--- app/components/ranking-chart.js
+++ app/components/ranking-chart.js
@@ -1,8 +1,7 @@
 import Ember from 'ember'; // eslint-disable-line
 import ResizeAware from 'ember-resize/mixins/resize-aware'; // eslint-disable-line
-
+import numeral from 'numeral';
 import d3 from 'd3';
-import landUseColors from '../utils/landUseColors';
 
 export default Ember.Component.extend(ResizeAware, {
   classNames: ['land-use-chart'],
@@ -12,6 +11,12 @@ export default Ember.Component.extend(ResizeAware, {
 
   data: [],
   column: '',
+
+  colors: {
+    gray: '#a8a8a8',
+    web_safe_orange: '#a24c0e',
+    dcp_orange: '#de7d2c',
+  },
 
   height: 50,
   margin: {
@@ -40,12 +45,12 @@ export default Ember.Component.extend(ResizeAware, {
     if (!div) {
       div = d3.select(el.get(0)).append('div')
         .attr('class', 'tooltip')
-        .style('opacity', 0);
+        .attr('style', 'opacity: 0;');
     }
 
     div
       .attr('class', 'tooltip')
-      .style('opacity', 0);
+      .attr('style', 'opacity: 0;');
 
     svg
       .attr('width', width + margin.left + margin.right)
@@ -64,6 +69,8 @@ export default Ember.Component.extend(ResizeAware, {
     const div = this.get('tooltipDiv');
     const height = this.get('height');
     const width = this.get('width');
+    const borocd = this.get('borocd');
+    const colorsHash = this.get('colors');
     const data = this.get('data');
     const column = this.get('column');
     const rank = this.get('rank');
@@ -74,47 +81,69 @@ export default Ember.Component.extend(ResizeAware, {
       const x = d3.scaleBand()
         .domain(data.map(d => d.borocd))
         .range([0, width])
-        .paddingOuter(0)
-        .paddingInner(0.2);
+        .padding(0);
 
       const y = d3.scaleLinear()
         .domain([0, d3.max(data, d => d[column])])
         .range([0, height]);
+
+      const colors = (d, i) => {
+        return (i === rank) ? colorsHash.web_safe_orange : colorsHash.gray;
+      };
+
+      const isCurrentlySelected = (i) => {
+        return rank === i;
+      };
 
       const bars = svg.selectAll('.bar')
         .data(data);
 
       bars.enter()
         .append('rect')
-        .attr('class', 'bar')
-        .attr('fill', function(d,i) {
-          return (i === rank) ? 'red' : 'blue';
-        })
-        .attr('y', 0)
-        .attr('width', 10)
-        .attr('rx', 2)
-        .attr('ry', 2)
+        .attr('class', (d,i) => `bar bar-${d.borocd}`)
+        .attr('fill', colors)
+        .attr('y', d => height - y(d[column]))
+        .attr('width', d => x.bandwidth() - 2)
         .attr('x', d => x(d.borocd))
-        .attr('height', d => y(d[column]))
-        .on("mouseover", function(d) {
-          console.log('test');
-          div.transition()
-            .duration(200)
-            .style('opacity', 0.9);
-          div.html('test')
-            .style('left', (d3.event.pageX) + 'px')
-            .style('top', (d3.event.pageY - 28) + 'px');
+        .attr('height', d => y(d[column]));
+
+      bars.enter()
+        .append('rect')
+        .attr('class', 'mask')
+        .attr('opacity', 0)
+        .attr('y', 0)
+        .attr('width', x.bandwidth())
+        .attr('x', d => x(d.borocd))
+        .attr('height', height)
+        .on('mouseover', function(d, i) {
+          const selector = `.bar-${d.borocd}`;
+          d3.selectAll(selector)
+            .transition()
+            .duration(10)
+            .attr('fill', colorsHash.dcp_orange);
+
+          div
+            // .transition()
+            // .duration(10)
+            .attr('style', 'opacity: 1;')
+            .attr('style', () => `left: ${x(d.borocd)}px`)
+            .text(() => `${d.boro_district}: ${numeral(d[column], '0.0')}%`);
         })
-        .on('mouseout', function(d) {
-          div.transition()
-            .duration(500)
-            .style('opacity', 0);
+        .on('mouseout', function(d, i) {
+          const selector = `.bar-${d.borocd}`;
+          d3.selectAll(selector)
+            .transition()
+            .duration(10)
+            .attr('fill', isCurrentlySelected(i) ? colorsHash.web_safe_orange : colorsHash.gray);
         });
 
+      svg.on('mouseout', function() {
+        div
+          .attr('style', 'opacity: 0;');
+      });
+
       bars.transition().duration(300)
-        .attr('fill', function(d,i) {
-          return (i === rank) ? 'red' : 'blue';
-        });
+        .attr('fill', colors);
 
       bars.exit().remove();
     });
