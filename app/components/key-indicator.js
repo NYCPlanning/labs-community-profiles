9b0602895de67bef1f4fe3c018625b739d657afb
allthesignals <wilburnforce@gmail.com>
2017-08-10 11:25:07 -0400

    Let data convey state about selection

--- app/components/key-indicator.js
+++ app/components/key-indicator.js
@@ -8,15 +8,18 @@ export default Ember.Component.extend({
   column: '',
   data: [],
 
-  sortedData: computed('data', function() {
+  sortedData: computed('data', 'borocd', function() {
+    const borocd = this.get('borocd');
     return this.get('data').then(data=> {
-      return data.sortBy(`${this.get('column')}`).reverse();
+      return data.sortBy(`${this.get('column')}`).reverse().map(d => {
+        d.is_selected = (borocd === d.borocd) ? true : false;
+        return d;
+      });
     });
   }),
 
   rankingIndex: computed('borocd', 'sortedData', function() {
     const borocd = this.get('borocd');
-
     return this.get('sortedData').then(sorted=> {
       return sorted.findIndex(el=>el.borocd === borocd);
     });
