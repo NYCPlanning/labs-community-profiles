1cc920bff806628680bf04f9c7d409ebdf82e8ea
Chris Whong <chris@whong.org>
2017-08-10 17:55:33 -0400

    refactor facmap as facsection, add domain counts

--- tests/integration/components/facilities-section-test.js
+++ tests/integration/components/facilities-section-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('facilities-map', 'Integration | Component | facilities map', {
+moduleForComponent('facilities-section', 'Integration | Component | facilities section', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{facilities-map}}`);
+  this.render(hbs`{{facilities-section}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#facilities-map}}
+    {{#facilities-section}}
       template block text
-    {{/facilities-map}}
+    {{/facilities-section}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
