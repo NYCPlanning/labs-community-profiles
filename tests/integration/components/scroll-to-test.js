ec575ebd80235a1611c66c41dcbcfc21afa95337
allthesignals <wilburnforce@gmail.com>
2017-08-10 23:31:42 -0400

    Smoke screen test

---
+++ tests/integration/components/scroll-to-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('scroll-to', 'Integration | Component | scroll to', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{scroll-to}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#scroll-to}}
+      template block text
+    {{/scroll-to}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
