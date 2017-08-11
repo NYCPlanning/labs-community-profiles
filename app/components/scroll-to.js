4fe8dbeb4c2e6c08335d1e06c4aa9085fa2233ff
allthesignals <wilburnforce@gmail.com>
2017-08-10 23:30:14 -0400

    Override addon to pass additional context

---
+++ app/components/scroll-to.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import ScrollTo from 'ember-scroll-to/components/scroll-to';
+
+export default ScrollTo.extend({
+  // ----- Events -----
+  scroll: Em.on('click', function(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    this
+      .get('scroller')
+      .scrollVertical(this.get('jQueryElement'), {
+        duration: this.get('duration'),
+        offset:   this.get('offset'),
+        easing:   this.get('easing'),
+        complete: () => Em.run(this, this.sendAction, 'afterScroll', this.get('href'), evt),
+      });
+  })
+});
