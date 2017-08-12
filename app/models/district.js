4e6d471abe9d418b0dc63494edf9190c87cfdf85
allthesignals <wilburnforce@gmail.com>
2017-08-11 21:47:21 -0400

    Setup District model
    This model imports all district-level data transformations and enables computed properties on district-only records.

--- app/models/district.js
+++ app/models/district.js
@@ -1,6 +1,18 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+import neighborhoodsCrosswalk from '../utils/nabesCrosswalk';
 
 export default DS.Model.extend({
-  borocd: DS.attr('string'),
+  borocd: DS.attr('number'),
+  boro: DS.attr('string'),
+  cd: DS.attr('string'),
   geometry: DS.attr(),
+  bounds: Ember.computed('geometry', function() {
+    return [];
+  }),
+  neighborhoods: Ember.computed('borocd', function() {
+    const borocd = this.get('borocd');
+    return neighborhoodsCrosswalk[borocd];
+  }),
+  dataprofile: {},
 });
