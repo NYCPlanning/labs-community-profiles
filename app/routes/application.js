4ceac334c0a3613ebb68e53248e9a09b8805e6f5
Chris Whong <chris@whong.org>
2017-08-14 17:50:54 -0400

    Add citywide getbounds to application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,20 @@
 import Ember from 'ember'; // eslint-disable-line
+import bbox from 'npm:@turf/bbox'; // eslint-disable-line
 import toGeojson from '../utils/to-geojson';
 
 export default Ember.Route.extend({
+  mapState: Ember.inject.service(),
+
   model() {
     return this.store.findAll('district');
   },
+
+  afterModel(districts) {
+    const mapState = this.get('mapState');
+    const geojsonDistricts = toGeojson(districts);
+    mapState.set('bounds', bbox(geojsonDistricts));
+  },
+
 
   setupController(controller, districts) {
     this._super(controller, districts);
