0db847f4482a806c4ca3237451dd5e2da95f193d
allthesignals <wilburnforce@gmail.com>
2017-08-15 10:02:43 -0400

    Re-add index bounding box
    Transition into application route triggers the application route lifecycle, but transitioning into other routes, specifically profiles, then transitioning back into application/index does not re-run the application route lifecycle. The top-level index route does re-run, however, so having this logic inside the top-level index route means the bounds will reset each time.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,19 @@
 import Ember from 'ember'; // eslint-disable-line
+import toGeojson from '../utils/to-geojson';
+import bbox from 'npm:@turf/bbox'; // eslint-disable-line
 
 export default Ember.Route.extend({
   mapState: Ember.inject.service(),
+
+  model() {
+    return this.modelFor('application');
+  },
+
+  afterModel(districts) {
+    const mapState = this.get('mapState');
+    const geojsonDistricts = toGeojson(districts);
+    mapState.set('bounds', bbox(geojsonDistricts));
+  },
 
   actions: {
     didTransition() {
