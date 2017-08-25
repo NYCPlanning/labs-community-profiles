f4b19ed7c01b77c7fa08d6300d592568446584a2
Chris Whong <chris@whong.org>
2017-08-25 15:02:26 -0400

    Implement 197a plans subsection

--- app/components/197a-plans.js
+++ app/components/197a-plans.js
@@ -3,9 +3,9 @@ import carto from '../utils/carto';
 
 export default Ember.Component.extend({
   district: null,
-  studies: Ember.computed('district', function() {
+  plans: Ember.computed('district', function() {
     const borocd = this.get('district.borocd');
-    const SQL = `SELECT * FROM cdprofiles_studies_plans WHERE cd LIKE '%25${borocd}%25'`;
+    const SQL = `SELECT * FROM cdprofiles_197a_plans WHERE cd LIKE '%25${borocd}%25'`;
     return carto.SQL(SQL, 'json');
   }),
 });
