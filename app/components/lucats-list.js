c2604f2282c74012211838fbc73509c604b8a183
Chris Whong <chris@whong.org>
2017-08-30 11:10:10 -0400

    Update lucats listing to use new API

--- app/components/lucats-list.js
+++ app/components/lucats-list.js
@@ -6,20 +6,25 @@ export default Ember.Component.extend({
   projects: Ember.computed('district', function() {
     const boro = this.get('district.boro').toLowerCase();
     const cd = this.get('district.cd');
-    const URL = `https://lucats.planninglabs.nyc/cdprojects/${boro}/${cd}`;
+    const URL = `https://lucats.planninglabs.nyc/urlurp/cd/${boro}/${cd}.json`;
 
     return fetch(URL)
       .then(data => data.json())
       .then((data) => {
-        data.active.forEach((d) => {
-          d.projectName = d.projectName !== '' ? d.projectName : 'Unnamed Project'; // eslint-disable-line
-        });
-
-        data.completed.forEach((d) => {
+        data.forEach((d) => {
           d.projectName = d.projectName !== '' ? d.projectName : 'Unnamed Project'; // eslint-disable-line
         });
 
         return data;
       });
+  }),
+  active: Ember.computed('projects', function() {
+    const projects = this.get('projects');
+    return projects.then(data => data.filter(d => d.status === 'active'));
+  }),
+
+  completed: Ember.computed('projects', function() {
+    const projects = this.get('projects');
+    return projects.then(data => data.filter(d => d.status === 'completed'));
   }),
 });
