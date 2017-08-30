d393fe527fbba8780299c088040054472df833ea
Chris Whong <chris@whong.org>
2017-08-29 21:56:49 -0400

    progress on map and chart

--- app/components/waterfront-chart.js
+++ app/components/waterfront-chart.js
@@ -1,7 +1,7 @@
 import Ember from 'ember'; // eslint-disable-line
 import ResizeAware from 'ember-resize/mixins/resize-aware'; // eslint-disable-line
-import fetch from 'fetch'; // eslint-disable-line
 
+import carto from '../utils/carto';
 import landUseColors from '../utils/landUseColors';
 
 const LandUseChart = Ember.Component.extend(ResizeAware, {
@@ -13,11 +13,52 @@ const LandUseChart = Ember.Component.extend(ResizeAware, {
   loading: false,
 
   borocd: '',
-
-  data: Ember.computed('borocd', function() {
+  sql: Ember.computed('borocd', function sql() {
     const borocd = this.get('borocd');
-    return fetch(`/data/floodplain_chart/${borocd}.json`)
-      .then(data => data.json());
+    const SQL = `
+      SELECT
+        building_typology,
+        totalbuildings,
+        totalunitsres,
+        SUM(numbldgs) as numbldgs,
+        SUM(unitsres) as unitsres,
+        ROUND(SUM(numbldgs)::numeric / totalbuildings, 4) AS numbldgs_pct,
+        ROUND(SUM(unitsres)::numeric / totalunitsres, 4) AS unitsres_pct
+      FROM (
+        SELECT
+          numbldgs,
+          unitsres,
+        CASE
+          WHEN (unitsres < 3 AND unitsres > 0) AND (proxcode = '0' OR proxcode = '1') THEN '1-2 family detached'
+          WHEN (unitsres < 3 AND unitsres > 0) AND (proxcode = '2' OR proxcode = '3') THEN '1-2 family attached/semi-detached'
+          WHEN (unitsres < 6 AND unitsres > 2) AND (numfloors < 5) AND (comarea = 0 AND officearea = 0 AND retailarea = 0 AND factryarea = 0) THEN 'Small Apartment Buildings'
+          WHEN (unitsres < 6 AND unitsres > 2) AND (comarea > 0 OR officearea > 0 OR retailarea > 0 OR factryarea > 0) THEN 'Small mixed-use buildings'
+          WHEN (unitsres = 0 AND numfloors < 5) AND (comarea > 0 OR officearea > 0 OR retailarea > 0) AND factryarea = 0 THEN 'Commercial-only buildings'
+          WHEN (unitsres = 0 AND numfloors < 5) AND factryarea > 0 THEN 'Manufacturing buildings'
+          WHEN (unitsres >= 6 AND numfloors > 4) AND (comarea = 0 AND officearea = 0 AND retailarea = 0 AND factryarea = 0) THEN 'Big apartment buildings'
+          WHEN (UnitsRes >= 6 AND NumFloors > 4) AND (ComArea > 0 OR OfficeArea > 0 OR RetailArea > 0) AND FactryArea = 0 THEN 'Big mixed-use buildings'
+          ELSE 'Other Building Types'
+        END AS building_typology,
+        SUM (numbldgs) OVER () as totalbuildings,
+        SUM (unitsres) OVER () as totalunitsres
+        FROM support_mappluto a
+        INNER JOIN support_admin_cdboundaries b
+        ON ST_Contains(b.the_geom, a.the_geom)
+        AND b.borocd = '${borocd}'
+        INNER JOIN support_waterfront_pfirm15 c
+        ON ST_Intersects(a.the_geom, c.the_geom)
+        AND (fld_zone = 'AE' OR fld_zone = 'VE')
+      ) x
+      GROUP BY building_typology, totalbuildings, totalunitsres
+      ORDER BY unitsres DESC
+    `;
+
+    return SQL;
+  }),
+
+  data: Ember.computed('sql', 'borocd', function() {
+    const sql = this.get('sql');
+    return carto.SQL(sql);
   }),
 
   didRender() {
@@ -45,7 +86,6 @@ const LandUseChart = Ember.Component.extend(ResizeAware, {
   updateChart: function updateChart() {
     const svg = this.get('svg');
     const data = this.get('data');
-    console.log('DATA', data);
 
     const el = this.$();
     const elWidth = el.width();
@@ -64,55 +104,53 @@ const LandUseChart = Ember.Component.extend(ResizeAware, {
       .attr('height', height + margin.top + margin.bottom);
 
     data.then((rawData) => {
+      console.log('rawData', rawData)
       const y = d3.scaleBand()
-        .domain(rawData.map(d => d.landuse_desc))
+        .domain(rawData.map(d => d.building_typology))
         .range([0, height])
         .paddingOuter(0)
-        .paddingInner(0.2);
+        .paddingInner(0.1);
 
       const x = d3.scaleLinear()
-        .domain([0, d3.max(rawData, d => d.percent)])
+        .domain([0, d3.max(rawData, d => d.unitsres)])
         .range([0, width]);
 
+      const typeLabels = svg.selectAll('.label')
+        .data(rawData, d => d.building_typology);
 
-      const bars = svg.selectAll('.bar')
-        .data(rawData, d => d.landuse);
-
-      bars.enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('fill', d => landUseColors(d.landuse))
-        .attr('x', 0)
-        .attr('height', y.bandwidth() - 15)
-        .attr('rx', 2)
-        .attr('ry', 2)
-        .attr('y', d => y(d.landuse_desc))
-        .attr('width', d => x(d.percent));
-
-
-      bars.transition().duration(300)
-        .attr('height', y.bandwidth() - 15)
-        .attr('y', d => y(d.landuse_desc))
-        .attr('width', d => x(d.percent));
-
-      bars.exit().remove();
-
-      const labels = svg.selectAll('text')
-        .data(rawData, d => d.landuse);
-
-      labels.enter().append('text')
+      typeLabels.enter().append('text')
         .attr('class', 'label')
         .attr('text-anchor', 'left')
         .attr('alignment-baseline', 'top')
+        .attr('x', 0);
+
+      typeLabels.transition().duration(300)
+        .attr('y', d => y(d.building_typology) + y.bandwidth() + -3)
+        .text(d => `${d.building_typology} | ${(d.unitsres_pct * 100).toFixed(1)} %`);
+
+      typeLabels.exit().remove();
+
+      const buildingsbars = svg.selectAll('.buildingsbar')
+        .data(rawData, d => d.building_typology);
+
+      buildingsbars.enter()
+        .append('rect')
+        .attr('class', 'buildingsbar')
+        .attr('fill', '#A8A8A8')
         .attr('x', 0)
-        .attr('y', d => y(d.landuse_desc) + y.bandwidth() + -3)
-        .text(d => `${d.landuse_desc} | ${(d.percent * 100).toFixed(2)}%`);
+        .attr('height', y.bandwidth() - 14)
+        .attr('rx', 2)
+        .attr('ry', 2)
+        .attr('y', d => y(d.building_typology))
+        .attr('width', d => x(d.unitsres));
 
-      labels.transition().duration(300)
-        .attr('y', d => y(d.landuse_desc) + y.bandwidth() + -3)
-        .text(d => `${d.landuse_desc} | ${(d.percent * 100).toFixed(2)}%`);
 
-      labels.exit().remove();
+      buildingsbars.transition().duration(300)
+        .attr('height', y.bandwidth() - 14)
+        .attr('y', d => y(d.building_typology))
+        .attr('width', d => x(d.unitsres));
+
+      buildingsbars.exit().remove();
     });
   },
 });
