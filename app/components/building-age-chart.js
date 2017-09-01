3166d5014ccfaaf993ca657c1ef3cab566727ebe
Chris Whong <chris@whong.org>
2017-08-31 23:58:22 -0400

    Remove SQL queries

--- app/components/building-age-chart.js
+++ app/components/building-age-chart.js
@@ -7,38 +7,6 @@ import githubraw from '../utils/githubraw';
 const BuildingAgeChart = Ember.Component.extend(ResizeAware, {
   classNames: ['relative'],
   borocd: '',
-  sql: Ember.computed('borocd', function sql() {
-    const borocd = this.get('borocd');
-    const SQL = `
-    SELECT
-      sum(numbldgs) as value, building_age as group,
-      ROUND(count(building_age)::numeric / totalbuildings, 4) AS value_pct
-    FROM (
-      SELECT
-      CASE
-        WHEN yearbuilt > 0 AND yearbuilt < 1961  THEN 'Pre-1961'
-        WHEN yearbuilt >= 1961 AND YearBuilt < 1983 THEN '1961-1982'
-        WHEN yearbuilt >= 1983 AND YearBuilt < 2013 THEN '1983-2012'
-        WHEN yearbuilt >= 2013 THEN '2013-Present'
-        ELSE 'Unknown'
-      END AS building_age,
-      numbldgs,
-      SUM (numbldgs) OVER () as totalbuildings
-      FROM support_mappluto a
-      INNER JOIN support_admin_cdboundaries b
-      ON ST_Contains(b.the_geom, a.the_geom)
-      AND b.borocd = '${borocd}'
-      INNER JOIN support_waterfront_pfirm15 c
-      ON ST_Intersects(a.the_geom, c.the_geom)
-      AND (fld_zone = 'AE' OR fld_zone = 'VE')
-    ) x
-    GROUP BY building_age, totalbuildings
-    ORDER BY array_position(array['Pre-1961','1961-1982','1983-2012','2013-Present','Unknown'], building_age)
-    `;
-
-    return SQL;
-  }),
-
   data: Ember.computed('sql', 'borocd', function() {
     const borocd = this.get('borocd');
     return githubraw('building_age', borocd);
--- app/components/zoning-chart.js
+++ app/components/zoning-chart.js
@@ -29,31 +29,6 @@ const LandUseChart = Ember.Component.extend(ResizeAware, {
   loading: false,
 
   borocd: '',
-  sql: Ember.computed('borocd', function sql() {
-    const borocd = this.get('borocd');
-    const SQL = `
-      WITH zones as (
-        SELECT ST_Intersection(ST_MakeValid(a.the_geom), ST_MakeValid(b.the_geom)) as the_geom, zonedist
-        FROM support_zoning_zd a, support_admin_cdboundaries b
-        WHERE ST_intersects(ST_MakeValid(a.the_geom), ST_MakeValid(b.the_geom))
-        AND b.borocd = '${borocd}'
-      ),
-      totalsm AS (
-        SELECT sum(ST_Area(the_geom::geography)) as total
-        FROM zones
-      )
-
-    SELECT sum(percent) as percent, zonedist FROM (
-        SELECT  ROUND((sum(ST_Area(the_geom::geography))/totalsm.total)::numeric,4) as percent, LEFT(zonedist, 1) as zonedist
-      FROM zones, totalsm
-      GROUP BY zonedist, totalsm.total
-      ORDER BY percent DESC
-    ) x
-    GROUP BY zonedist
-  `;
-
-    return SQL;
-  }),
 
   data: Ember.computed('sql', 'borocd', function() {
     const borocd = this.get('borocd');
