c44e2214f334a0115f50e16e8e35201ad2ce689b
Chris Whong <chris@whong.org>
2017-08-31 23:58:54 -0400

    remove zoning sql

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -29,31 +29,6 @@ function buildBorocd(boro, cd) {
   return borocode + parseInt(cd, 10);
 }
 
-function generateZoningSQL(borocd) {
-  const SQL = `
-    WITH zones as (
-      SELECT ST_Intersection(ST_MakeValid(a.the_geom), ST_MakeValid(b.the_geom)) as the_geom, zonedist
-      FROM support_zoning_zd a, support_admin_cdboundaries b
-      WHERE ST_intersects(ST_MakeValid(a.the_geom), ST_MakeValid(b.the_geom))
-      AND b.borocd = '${borocd}'
-    ),
-    totalsm AS (
-      SELECT sum(ST_Area(the_geom::geography)) as total
-      FROM zones
-    )
-
-  SELECT sum(percent) as percent, zonedist FROM (
-      SELECT  ROUND((sum(ST_Area(the_geom::geography))/totalsm.total)::numeric,4) as percent, LEFT(zonedist, 1) as zonedist
-    FROM zones, totalsm
-    GROUP BY zonedist, totalsm.total
-    ORDER BY percent DESC
-  ) x
-  GROUP BY zonedist
-`;
-
-  return SQL;
-}
-
 export default Ember.Route.extend(ScrollToTop, {
 
   mapState: Ember.inject.service(),
