ea1428e851660789a270e7754b25df2aa78fa97e
allthesignals <wilburnforce@gmail.com>
2017-12-18 12:32:48 -0500

    Change carto endpoint

--- app/utils/carto.js
+++ app/utils/carto.js
@@ -1,15 +1,14 @@
 import fetch from 'fetch'; // eslint-disable-line
 import { Promise } from 'rsvp'; // eslint-disable-line
 
-const cartoUser = 'data';
-const cartoDomain = 'carto.planninglabs.nyc';
+const cartoDomain = 'planninglabs.carto.com';
 
 const buildTemplate = (layergroupid, type) => { // eslint-disable-line
-  return `https://${cartoDomain}/user/${cartoUser}/api/v1/map/${layergroupid}/{z}/{x}/{y}.${type}`;
+  return `https://${cartoDomain}/api/v1/map/${layergroupid}/{z}/{x}/{y}.${type}`;
 };
 
 const buildSqlUrl = (cleanedQuery, type = 'json') => { // eslint-disable-line
-  return `https://${cartoDomain}/user/${cartoUser}/api/v2/sql?q=${cleanedQuery}&format=${type}`;
+  return `https://${cartoDomain}/api/v2/sql?q=${cleanedQuery}&format=${type}`;
 };
 
 const carto = {
@@ -46,7 +45,7 @@ const carto = {
     };
 
     return new Promise((resolve, reject) => {
-      fetch(`https://${cartoDomain}/user/${cartoUser}/api/v1/map`, {
+      fetch(`https://${cartoDomain}/api/v1/map`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
