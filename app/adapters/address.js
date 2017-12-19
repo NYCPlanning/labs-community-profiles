308ed1c66f5c58edf6a308aa4356640aaae3002e
Chris Whong <chris@whong.org>
2017-12-19 12:57:58 -0500

    update mapzen key

--- app/adapters/address.js
+++ app/adapters/address.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-const mapzenSearchAPI = 'https://search.mapzen.com/v1/autocomplete?focus.point.lat=40.7259&focus.point.lon=-73.9805&limit=5&api_key=mapzen-y89dXNc&text=';
+const mapzenSearchAPI = 'https://search.mapzen.com/v1/autocomplete?focus.point.lat=40.7259&focus.point.lon=-73.9805&limit=5&api_key=mapzen-XFqSbzh&text=';
 
 export default DS.JSONAPIAdapter.extend({
   keyForAttribute(key) {
