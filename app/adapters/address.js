4adcb3a574320f9eae197a91e37df916b52b2887
Chris Whong <chris@whong.org>
2018-01-29 12:14:54 -0500

    remove merge conflict

--- app/adapters/address.js
+++ app/adapters/address.js
@@ -1,10 +1,6 @@
 import DS from 'ember-data';
 
-<<<<<<< HEAD
 const mapzenSearchAPI = 'https://search.mapzen.com/v1/autocomplete?focus.point.lat=40.7259&focus.point.lon=-73.9805&limit=5&api_key=mapzen-YuCycbW&text=';
-=======
-const mapzenSearchAPI = 'https://search.mapzen.com/v1/autocomplete?focus.point.lat=40.7259&focus.point.lon=-73.9805&limit=5&api_key=mapzen-y89dXNc&text=';
->>>>>>> 6320bb8698c9fdb266a734ab34f4df383e79ae97
 
 export default DS.JSONAPIAdapter.extend({
   keyForAttribute(key) {
