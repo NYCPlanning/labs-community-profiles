9f4679b5979eca3d28c784a02ff11f5ff6d992e9
allthesignals <wilburnforce@gmail.com>
2018-01-31 14:32:11 -0500

    Override matcher; show alll available address options

--- app/components/navigation-dropdown.js
+++ app/components/navigation-dropdown.js
@@ -1,10 +1,11 @@
 import { isBlank } from '@ember/utils';
 import { A } from '@ember/array';
-import { computed } from '@ember/object';
+import { computed, get } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import { Promise } from 'rsvp';
 import { task, timeout } from 'ember-concurrency';
+import { defaultMatcher} from 'ember-power-select/utils/group-utils';
 
 const DEBOUNCE_MS = 250;
 
@@ -38,6 +39,15 @@ export default Component.extend({
     yield timeout(DEBOUNCE_MS);
     yield this.set('searchTerms', terms);
   }).restartable(),
+
+  matcher(value, searchTerm) {
+    if (get(value, 'constructor.modelName') === 'address') {
+      return true;
+    }
+
+    return defaultMatcher(get(value, 'name'), searchTerm);
+  },
+
   actions: {
     handleSearch(terms, dropdownState) {
       if (dropdownState.resultsCount <= 1) {
