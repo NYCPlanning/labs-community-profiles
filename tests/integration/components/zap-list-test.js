afae2ad68bf55f4bcad8556cc064569f502103d9
Chris Whong <chris@whong.org>
2018-04-30 15:50:05 -0400

    add zap-list component

---
+++ tests/integration/components/zap-list-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('zap-list', 'Integration | Component | zap list', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{zap-list}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#zap-list}}
+      template block text
+    {{/zap-list}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
