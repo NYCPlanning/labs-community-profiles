0adc64018ed9cee2e857455fab5c3da1716c8af5
Chris Whong <chris@whong.org>
2018-07-19 23:52:23 -0400

    consume zap api

--- app/components/zap-list.js
+++ app/components/zap-list.js
@@ -8,9 +8,23 @@ export default Component.extend({
   projects: computed('district', function() {
     const zapAcronym = this.get('district.zapAcronym');
 
-    const URL = `https://zap-api.planninglabs.nyc/zap/${zapAcronym}.json`;
+    const URL = `https://zap-api.planninglabs.nyc/projects?community-districts[]=${zapAcronym}&dcp_publicstatus[]=Filed&dcp_publicstatus[]=In Public Review&page=1`;
 
     return fetch(URL)
-      .then(data => data.json());
+      .then(res => res.json())
+      .then((res) => {
+        // get the data object, return object with arrays of projects grouped by dcp_publicstatus_simp
+        const projects = res.data;
+
+        projects.forEach((project) => {
+          const applicant = project.attributes.applicants.split(';')[0];
+          project.attributes.applicant = applicant; // eslint-disable-line
+        });
+
+        return {
+          filed: projects.filter(d => d.attributes.dcp_publicstatus_simp === 'Filed'),
+          inPublicReview: projects.filter(d => d.attributes.dcp_publicstatus_simp === 'In Public Review'),
+        };
+      });
   }),
 });
