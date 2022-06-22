4f5871e8db6c565f528af6850b8ca17d85155306
Horatio <horatiorosa@gmail.com>
2022-06-22 15:36:21 -0400

    Revert "818: update language for acs tooltips (remove puma references) and indicators methodology"
    This reverts commit 6c55bff91f8c2a3d28e964807255272af9a6febd.

--- app/helpers/acs-floodplain-tooltip.js
+++ app/helpers/acs-floodplain-tooltip.js
@@ -3,12 +3,14 @@ import { tooltipText } from '../tooltips/tooltip-text';
 
 export function buildTooltip(data) {
   const {
+    puma,
     cd_short_title,
   } = data[0];
 
-  const acsFloodplain = tooltipText.acs.acsFloodplain(cd_short_title);
+  const acsFloodplain = tooltipText.acs.acsFloodplain(puma);
+  const cdApproximation = tooltipText.acs.cdApproximation(cd_short_title);
 
-  return acsFloodplain;
+  return acsFloodplain.concat(cdApproximation);
 }
 
 export default helper(buildTooltip);
