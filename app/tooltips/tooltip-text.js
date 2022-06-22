c0a89575d658f3679ff593d350b1305030e2d3e7
Horatio <horatiorosa@gmail.com>
2022-06-22 15:36:21 -0400

    Revert "818: update language for acs tooltips (remove puma references) and indicators methodology"
    This reverts commit 6c55bff91f8c2a3d28e964807255272af9a6febd.

--- app/tooltips/tooltip-text.js
+++ app/tooltips/tooltip-text.js
@@ -1,10 +1,16 @@
 export const tooltipText = {
   acs: {
-    communityDistrict(cd_short_title) {
-      return `American Community Survey 2014-2018 5-Year Estimates for ${cd_short_title}, approximated by aggregating data from blocks and block-groups.`;
+    puma(puma) {
+      return `American Community Survey 2014-2018 5-Year Estimates for PUMA ${puma}, `;
     },
-    acsFloodplain(cd_short_title) {
-      return `American Community Survey (ACS) 2014-2018 5-Year Estimates for floodplain area within ${cd_short_title}, approximated by aggregating data from blocks and block-groups.`;
+    cdApproximation(cd_short_title) {
+      return `which is an approximation of ${cd_short_title}.`;
+    },
+    cdApproximationShared(cd_short_title, shared_puma_cd) {
+      return `which is an approximation of both ${cd_short_title} and ${shared_puma_cd}.`;
+    },
+    acsFloodplain(puma) {
+      return `American Community Survey (ACS) 2013-2017 5-year estimates for floodplain area within PUMA ${puma}, `; // which is an approximation of ${cd_short_title}
     },
   },
   census: {
