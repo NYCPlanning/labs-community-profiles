65a73d351e6170b51d0264091bb579ce777f4966
Tyler Matteo <tylermatteo434@gmail.com>
2022-06-23 17:32:51 -0400

    Update facilities queries to use correct Carto table

--- app/components/facilities-section.js
+++ app/components/facilities-section.js
@@ -6,7 +6,7 @@ import geoViewport from '@mapbox/geo-viewport'; // eslint-disable-line
 
 import carto from '../utils/carto';
 
-const facdbTable = 'facdb';
+const facdbTable = 'dcp_facilities';
 
 const colorsArray = [
   ['Education, Child Welfare, and Youth', '#f7ca00'],
