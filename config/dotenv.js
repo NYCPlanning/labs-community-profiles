e0fab3753d7d947fd972df09899275db3e8e7145
horatio <horatiorosa@aol.com>
2022-08-30 10:06:10 -0400

    update dotenv.js to allow fornew ENV key name

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 /* eslint-disable-next-line no-unused-vars */
 module.exports = function(env) {
   return {
-    clientAllowedKeys: ['ZAP_API_STAGING_URL', 'ZAP_API_PRODUCTION_URL'],
+    clientAllowedKeys: ['ZAP_API_URL'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env'),
