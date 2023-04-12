227a25e54108b98e9a5005f2cbb974bace5f6e89
Tim Miller <miller.tim108@gmail.com>
2023-04-12 13:57:57 -0400

    v3.19.0...v3.28.0; power select and map layer id errors

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,11 @@
+import Application from 'labs-community-portal/app';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
-import Application from '../app';
 
 setApplication(Application.create({ autoboot: false }));
+
+setup(QUnit.assert);
 
 start();
